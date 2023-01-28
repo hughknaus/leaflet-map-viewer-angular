ea9ad42daa02684493fa4d291937106804fc2c88
Hugh Knaus <hknaus@corelogic.com>
2023-01-27 21:25:05 -0700

    WIP

--- src/app/map-viewer-component/map-viewer.component.ts
+++ src/app/map-viewer-component/map-viewer.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { geoJson } from 'leaflet';
-import { mergeMap, Observable, of } from 'rxjs';
+import { mergeMap, Observable, of, reduce } from 'rxjs';
 declare const L:any;
 
 @Component({
@@ -10,8 +10,7 @@ declare const L:any;
   styleUrls: ['./map-viewer.component.scss']
 })
 export class MapViewerComponent implements OnInit {
-  leafletTestText = 'Leaflet Map Viewer Prototype';
-
+  leafletTestText = 'MapViewerComponent Prototype';
   private osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -22,12 +21,12 @@ export class MapViewerComponent implements OnInit {
   };
 
   private geoJsonAssets: any[] = [
-    { friendlyName: 'States', path: 'assets/cb_2017_us_state_500k_0.json', style: { fillColor: "#0000FF", weight: 2, fillOpacity: 0.15 } },
-    { friendlyName: 'West Region', path: 'assets/cb_2017_us_state_500k_west_1.json', style: { fillColor: "#780000", weight: 2, fillOpacity: 0.65 } },
-    { friendlyName: 'East Region', path: 'assets/cb_2017_us_state_500k_east_2.json', style: { fillColor: "#008080", weight: 2, fillOpacity: 0.65 } },
-    { friendlyName: 'Midwest Region', path: 'assets/cb_2017_us_state_500k_midwest_3.json', style: { fillColor: "#FFC300", weight: 3, fillOpacity: 0.65 } },
+    { friendlyName: 'Salvage Yards', path: 'assets/SalvageYardsRegions_5.json', style: { fillColor: "yellow", weight: 2, fillOpacity: 0.85 } }, 
     { friendlyName: 'Counties', path: 'assets/cb_2017_us_county_5m_4.json', style: { fillColor: "#0000FF", weight: 2, fillOpacity: 0.15 } },
-    { friendlyName: 'Salave Yards', path: 'assets/SalvageYardsRegions_5.json', style: { fillColor: "#0000FF", weight: 2, fillOpacity: 0.15 } }, 
+    { friendlyName: 'Midwest Region', path: 'assets/cb_2017_us_state_500k_midwest_3.json', style: { fillColor: "#FFC300", weight: 3, fillOpacity: 0.25 } },
+    { friendlyName: 'East Region', path: 'assets/cb_2017_us_state_500k_east_2.json', style: { fillColor: "#008080", weight: 2, fillOpacity: 0.25 } },
+    { friendlyName: 'West Region', path: 'assets/cb_2017_us_state_500k_west_1.json', style: { fillColor: "#780000", weight: 2, fillOpacity: 0.25 } },
+    { friendlyName: 'States', path: 'assets/cb_2017_us_state_500k_0.json', style: { fillColor: "#0000FF", weight: 2, fillOpacity: 0.15 } },
   ];
    
   constructor(private httpClient: HttpClient) {
@@ -36,12 +35,12 @@ export class MapViewerComponent implements OnInit {
   ngOnInit() {
     let map = L.map('map', {
       center: [39.73, -104.99],
-      zoom: 10,
+      zoom: 5,
       layers: [this.osm]
     });
 
-    var layerGroup = new L.LayerGroup();
-    layerGroup.addTo(map);
+    // var layerGroup = new L.LayerGroup();
+    // layerGroup.addTo(map);
 
     const layerControl = L.control.layers(this.baseMaps).addTo(map);
 
@@ -49,13 +48,39 @@ export class MapViewerComponent implements OnInit {
     //   .addTo(map)
     //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     //   .openPopup();
-    
+
     this.geoJsonAssets.forEach(item => {
       this.getJSON(item).subscribe(result => {
-        const geoJsonLayer = L.geoJSON().addTo(map);
-        geoJsonLayer.addData(result.data);
-        geoJsonLayer.setStyle(item.style);
-        layerGroup.addLayer(geoJsonLayer);
+        const options = {
+          pointToLayer: (feature, latLng) => { 
+            if (feature.geometry.type === 'Point') { 
+              let iconOptions = {
+                radius: 7,
+                color: 'goldenrod',
+                weight: 2,
+                opacity: 1.0,
+                fill: true,
+                fillColor: 'yellow',
+                fillOpacity: 0.8,
+                fillRule: 'evenodd',
+                renderer: L.svg(),
+                interactive: true
+              };
+              return L.circleMarker(latLng, iconOptions)
+                .bindPopup(`<div>
+                  <b>${feature.properties["Salvage Yard"]}</b><br />
+                  ${feature.properties["Address"]}<br />
+                  ${feature.properties["City"]}, 
+                  ${feature.properties["State"]} 
+                  ${feature.properties["Zip"]}
+                  </div>`)
+                .openPopup();
+            }
+          },
+          style: (feature) => result.style,
+        };
+        const geoJsonLayer = L.geoJSON(result.data, options).addTo(map);
+        //layerGroup.addLayer(geoJsonLayer);
         layerControl.addOverlay(geoJsonLayer, result.group);
       });
     });
@@ -63,7 +88,7 @@ export class MapViewerComponent implements OnInit {
 
   public getJSON(geoJsonAsset: any): Observable<any> {
     return this.httpClient.get(geoJsonAsset.path).pipe(
-      mergeMap(data => { return of({ group: geoJsonAsset.friendlyName, data: data })})
+      mergeMap(data => { return of({ group: geoJsonAsset.friendlyName, data: data, style: geoJsonAsset.style })})
     );
   }
 }
